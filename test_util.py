b20c35bd18dd9906d460f1873520d3ba4545f64b
Erik Garrett <thedevsandbox@gmail.com>
2019-06-25 16:19:39 -0700

    a test change

--- test_util.py
+++ test_util.py
@@ -3,6 +3,9 @@ import util
 
 class TestUtilMethods(unittest.TestCase):
   
+  def test_foo(self):
+    self.assertEqual(util.foo(), 'foobar')
+    
   def test_reverse_string(self):    
     self.assertEqual(util.reverse_string('foo'), 'oof')
     
