2d90ee1d23d58167846267b9bd9d214b5a3dad63
Erik Garrett <erik.garrett@mec.ca>
2019-06-25 09:35:10 -0700

    added source files.

---
+++ test_util.py
@@ -0,0 +1,10 @@
+import unittest
+import util
+
+class TestUtilMethods(unittest.TestCase):
+  
+  def test_reverse_string(self):    
+    self.assertEqual(util.reverse_string('foo'), 'oof')
+    
+if __name__ == '__main__':
+  unittest.main()    
---
+++ util.py
@@ -0,0 +1,3 @@
+
+def reverse_string(str):
+  return str[::-1]
