d4efd19f26fa2c0b1d49354e15f70087df1e55a0
thedevsandbox <thedevsandbox@gmail.com>
2019-06-26 10:13:15 -0700

    a change (#1)
    * a change
    
    * a test change
    
    * Added breaking change.
    
    * Fixed broken test.

--- test_util.py
+++ test_util.py
@@ -3,6 +3,9 @@ import util
 
 class TestUtilMethods(unittest.TestCase):
   
+  def test_randomize_string(self):
+    self.assertNotEqual(util.randomize_string('foobar'), 'foobar')
+    
   def test_reverse_string(self):    
     self.assertEqual(util.reverse_string('foo'), 'oof')
     
--- util.py
+++ util.py
@@ -1,3 +1,7 @@
+import random
 
+def randomize_string(str):
+  return ''.join(random.sample(str,len(str)))
+  
 def reverse_string(str):
   return str[::-1]
