4c9cb8a1134e78f5f0f4d523ce3b76457e3c1511
Erik Garrett <thedevsandbox@gmail.com>
2019-06-28 10:34:01 -0700

    Updated deploy script.

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@ echo "Installing serverless"
 echo "_______________________________"
 
 npm install -g serverless
-#npm install serverless-dynamodb-local serverless-offline serverless-stack-output
 
 echo "Deploying to $env"
 echo "_______________________________"
